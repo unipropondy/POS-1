ca29bf755c4b4bb9c25704bc05d9bf169fdf154d
Javith06 <miistahjavith@gmail.com>
2026-04-13 17:03:09 +0530

    Railway

--- frontend/constants/Config.tsx
+++ frontend/constants/Config.tsx
@@ -1,1 +1,1 @@
-export const API_URL = process.env.EXPO_PUBLIC_API_URL || "http://192.168.0.103:3000";
+export const API_URL = process.env.EXPO_PUBLIC_API_URL || "https://pos-1-production-b061.up.railway.app";
