ab0a74f3f128f96fc72422a43fedb3f6e8c552d9
Javith06 <miistahjavith@gmail.com>
2026-04-27 12:43:59 +0530

    Fix ImagePicker deprecation warning

--- frontend/app/company-settings.tsx
+++ frontend/app/company-settings.tsx
@@ -89,7 +89,7 @@ export default function CompanySettingsScreen() {
     }
 
     const result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      mediaTypes: ImagePicker.MediaType.Images,
       allowsEditing: true,
       aspect: [1, 1],
       quality: 0.7,
